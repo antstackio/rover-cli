b5a0b4c1485a14e36f89607c492e1e4f2b1aac5c
Dheerajgbhatt <79825980+DheerajGBhatt@users.noreply.github.com>
2022-09-28 18:20:58 +0530

    removed Math.random for string generation

--- cli-main/util.ts
+++ cli-main/util.ts
@@ -560,12 +560,6 @@ export let samValidate=function(){
   
 }
 export let makeid =function (length) {
-  var result           = '';
-  var characters       = 'abcdefghijklmnopqrstuvwxyz';
-  var charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() * 
-charactersLength));
- }
- return result;
+  const crypto = require('crypto');
+  return String.fromCharCode(...crypto.randomBytes(2))
 }
