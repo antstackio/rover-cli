3caaac2f27043cc9373aac439a606f2a322d4bd7
Dheerajgbhatt <79825980+DheerajGBhatt@users.noreply.github.com>
2022-09-28 21:48:41 +0530

    replaced [0-9] with \d in regex

--- cli-main/util.ts
+++ cli-main/util.ts
@@ -554,5 +554,5 @@ export let samValidate=function(){
 }
 export let makeid =function (length) {
   const crypto = require('crypto');
-  return (crypto.randomBytes(1).toString("base64url").replace(/[0-9]/g, 'd')).toLowerCase();
+  return (crypto.randomBytes(1).toString("base64url").replace(/\d/g, 'd')).toLowerCase();
 }
